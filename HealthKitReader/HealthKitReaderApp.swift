8cb6ff553eb982c2e7f0c86929c5156bb1c8d04b
Brahim Berkasse <berkassebrahim@gmail.com>
2023-01-03 14:41:56 +0100

    Initial Commit

---
+++ HealthKitReader/HealthKitReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitReaderApp.swift
+//  HealthKitReader
+//
+//  Created by Brahim Berkasse on 3/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
